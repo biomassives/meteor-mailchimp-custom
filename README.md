ba6cd695a2dd181f7a4412fc11bd9372adef0ac8
G. Willson <greg@biomassiv.es>
2015-02-26 12:23:53 -0700

    initial setup

---
+++ README.md
@@ -0,0 +1,5 @@
+METEOR Mailchimp API lib with Custom Fields
+
+
+documentation: Readme.md
+git repository: http://github.com/biomassives/meteor-mailchimp-custom
---
+++ biomassives:mailchimp-custom.js
@@ -0,0 +1,33 @@
+// Biomassives Group MIT licensed Meteor Package
+// http://github.com/biomassives/meteor-mailchimp-custom
+
+Package.describe({
+        name    : 'miro:mailchimp',
+        version : '1.0.4',
+        summary : 'A Meteor wrapper for MailChimp API',
+        homepage: "http://mailchimp.meteor.com",
+        author  : "Miroslav Hibler (http://miro.hibler.me)",
+        git     : 'https://github.com/MiroHibler/meteor-mailchimp.git'
+});
+
+Package.onUse( function ( api, where ) {
+
+        api.versionsFrom('METEOR@0.9.2');
+
+        api.use( ['templating'], 'client' );
+
+        api.addFiles( 'lib/server/mailchimp.js', 'server' );
+
+        api.addFiles([
+                'lib/client/views/subscribe/subscribe.html',
+                'lib/client/views/subscribe/subscribe.js',
+                'lib/client/mailchimp.js'
+        ], 'client' );
+
+        if ( api.export ) {
+                api.export( 'MailChimp', ['server', 'client'] );
+        }
+});
+
+Npm.depends({ 'mailchimp': '1.1.0' });
+
