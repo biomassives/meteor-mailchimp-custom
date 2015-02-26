367d00646f816068598c6b8f8bab5e1c0de1ab0d
G. Willson <greg@biomassiv.es>
2015-02-26 13:05:43 -0700

    version #

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'biomassives:mailchimp-custom',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'This package extends the Mailchip meteor package by simplifying dialogue and form customizations',
   // URL to the Git repository containing the source code for this package.
